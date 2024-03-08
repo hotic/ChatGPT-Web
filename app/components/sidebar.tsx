9a8e1df950f77538e5fd4c877ceef5277673d18d
Bkm016 <sky@asgard.me>
2024-03-09 01:44:58 +0800

    更新图标

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -174,10 +174,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          Asgard GPT
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          真是有意思啊。
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
